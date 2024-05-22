1a66d45e76035446e1084a4b8e76ce5e9e9cd14d
Eugene Michkov <michkov.evgeny@gmail.com>
2024-05-23 00:44:27 +0300

    update images

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -9,8 +9,7 @@ const RecentProjects = () => {
   return (
     <section className="py-20" id="projects">
       <h1 className="heading">
-        A small selection of{" "}
-        <span className="text-purple">recent projects</span>
+        Небольшая выборка <span className="text-purple">недавних проектов</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map((item) => (
@@ -18,10 +17,7 @@ const RecentProjects = () => {
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
             key={item.id}
           >
-            <PinContainer
-              title="/ui.aceternity.com"
-              href="https://twitter.com/mannupaaji"
-            >
+            <PinContainer title={item.link} href={item.link}>
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
@@ -32,7 +28,7 @@ const RecentProjects = () => {
                 <img
                   src={item.img}
                   alt="cover"
-                  className="z-10 absolute bottom-0"
+                  className="z-10 absolute bottom-0 transform rotate-3"
                 />
               </div>
 
@@ -67,7 +63,7 @@ const RecentProjects = () => {
 
                 <div className="flex justify-center items-center">
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check Live Site
+                    {item.text}
                   </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
--- components/ui/Pin.tsx
+++ components/ui/Pin.tsx
@@ -1,8 +1,9 @@
 "use client";
-import React, { useState } from "react";
 import { motion } from "framer-motion";
+import React, { useState } from "react";
 
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export const PinContainer = ({
   children,
@@ -12,8 +13,8 @@ export const PinContainer = ({
   containerClassName,
 }: {
   children: React.ReactNode;
-  title?: string;
-  href?: string;
+  title: string;
+  href: string;
   className?: string;
   containerClassName?: string;
 }) => {
@@ -37,7 +38,7 @@ export const PinContainer = ({
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
     >
-      <div
+      <Link href={href}
         style={{
           perspective: "1000px",
           transform: "rotateX(70deg) translateZ(0deg)",
@@ -53,35 +54,25 @@ export const PinContainer = ({
         >
           <div className={cn(" relative z-50 ", className)}>{children}</div>
         </div>
-      </div>
-      <PinPerspective title={title} href={href} />
+      </Link>
+      <PinPerspective title={title}/>
     </div>
   );
 };
 
-export const PinPerspective = ({
-  title,
-  href,
-}: {
-  title?: string;
-  href?: string;
-}) => {
+export const PinPerspective = ({ title }: { title: string }) => {
   return (
     // change w-96 to w-full
     <motion.div className="pointer-events-none w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
-          <a
-            href={href}
-            target={"_blank"}
-            className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
-          >
+          <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
             <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
               {title}
             </span>
 
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
-          </a>
+          </div>
         </div>
 
         <div
