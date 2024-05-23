a40b7478cd69723c60e71c1f279f260602abb2fb
Eugene Michkov <michkov.evgeny@gmail.com>
2024-05-23 21:11:37 +0300

    updated meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,41 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Michkoff | Portfolio",
+  icons: [
+    {
+      rel: "icon",
+      url: "/brand/m.svg",
+    },
+  ],
+  description:
+    "Евгений Мичков - Fullstack разработчик с особым подходом к своему делу",
+  openGraph: {
+    title: "Michkoff | Portfolio",
+    description:
+      "Евгений Мичков - Fullstack разработчик с особым подходом к своему делу",
+    type: "website",
+    url: "https://michkoff.com",
+    images: [
+      {
+        url: "/open-graph-banner.png",
+        width: 1200,
+        height: 630,
+        alt: "Michkoff",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Michkoff | Portfolio",
+    images: [
+      {
+        url: "/open-graph-banner.png",
+        width: 1200,
+        height: 630,
+        alt: "Michkoff",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
